7468774d4e02491aa242b2434c84c2b173e858ee
sickcurse <111002422+sickcurse@users.noreply.github.com>
2026-06-01 00:35:24 -0400

    really almost

--- server/index.js
+++ server/index.js
@@ -3,24 +3,20 @@ const cors    = require('cors')
 const path    = require('path')
 const { init } = require('./db')
 
-const app  = express()
-const prod = process.env.NODE_ENV === 'production'
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-if (prod) {
-  const clientDist = path.join(__dirname, '../client/dist')
-  console.log('Serving static from:', clientDist)
-  app.use(express.static(clientDist))
-
-  app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  })
-}
+const clientDist = path.join(__dirname, '../client/dist')
+app.use(express.static(clientDist))
 
 app.use('/api/auth',   require('./routes/auth'))
 app.use('/api/scores', require('./routes/scores'))
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 const PORT = process.env.PORT || 3000
 
